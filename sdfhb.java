a58d29575eb3f2c203ecc6c4543c4d855e9d2595
123456 <140791320+lsksf@users.noreply.github.com>
2024-04-13 16:59:45 +0800

    Create sdfhb.java

---
+++ sdfhb.java
@@ -0,0 +1,16 @@
+public class Test { public static void main ( String [ ] args ) { int [ ] numbers = { 10,20,30,40,50 } ;​​​​​​​​
+ 
+       for ( int x :数字) { if ( x == 30 ) {继续;
+          }系统。出去。打印（x ）；
+         系统。出去。打印（“ \n ” ）；
+       } } }   
+public class Test {
+ 
+   public static void main(String args[]){
+      int x = 10;
+ 
+      if( x < 20 ){
+         System.out.print("这是 if 语句");
+      }
+   }
+}
